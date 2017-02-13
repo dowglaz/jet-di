d7967dce4b8466f72198893ab7af4f2dd3eabfde
Douglas Meira Cotrim Pereira de Melo <cotrim.doug@gmail.com>
2017-02-12 23:53:55 -0200

    WIP - refactoring

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
+// External dependencies
 const cloneDeep = require('lodash/cloneDeep');
 const pick = require('lodash/pick');
 const readPkgUp = require('read-pkg-up');
 
-const Context = require('./lib/Context');
+// Local dependencies
 const Registry = require('./lib/Registry');
+const Context = require('./lib/Context');
 const functions = require('./lib/functions');
 const setupContext = require('./lib/setupContext');
 
-const localRegistry = new Registry();
+// Local instantiations
 const externalRegistry = new Registry();
+const localRegistry = new Registry();
 const globalRegistry = new Registry(global);
 
 const packageConfigs = pick(
   readPkgUp.sync().pkg, 'dependencies', 'depMap', 'globalDependencies');
 
+/**
+ * Exposes a function that needs the local (user) require to work.
+ * It uses the local require function to load local project's external
+ * and local dependencies into the indicated local classes.
+ */
 module.exports = localRequire => (
   setupContext({
     Context,
